8e068afd2d4b27f7555472db5cbf047c32f46683
Usama Sohail <135988912+usama200348@users.noreply.github.com>
2026-08-06 14:46:40 +0500

    Vercel File Added

--- Frontend/Shop_pak_ecom_mern/src/pages/Cart.jsx
+++ Frontend/Shop_pak_ecom_mern/src/pages/Cart.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import {
-  FaTrash,
-  FaPlus,
-  FaMinus,
-  FaShoppingCart,
-  FaArrowLeft,
-} from "react-icons/fa";
+import {FaTrash,FaPlus,FaMinus,FaShoppingCart,FaArrowLeft} from "react-icons/fa";
 import {
   removeFromCart,
   increaseQty,
